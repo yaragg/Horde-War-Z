3048e42e0605bb0b0bd8d8ee8b3b007f3846fa51
Zachary Cupec <zcupec@gmail.com>
2016-02-19 14:52:07 -0500

    Player and Zombie
    Added basic movement for the player and zombie. End of class 2/19

---
+++ Assets/Scripts/AIMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovement : MonoBehaviour {
+
+    public float moveSpeed = 6.0f;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Vector3 toPlayer = new Vector3();
+
+        toPlayer = player.transform.position - this.transform.position;
+        this.transform.Translate(toPlayer.normalized * moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+
+        this.transform.Translate(new Vector3(h , v, 0) * moveSpeed * Time.deltaTime);
+	}
+}
