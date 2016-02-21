c48f4bd9c99fc011d4d3b32e65ba8c0ebe6a80b7
Yara Grassi Gouffon <yara.gouffon@usp.br>
2016-02-21 17:52:04 -0500

    Added health tracking to zombies and characters. Characters have four red circles under them that turn grayish as they take damage.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+	public int health = 4;
+	public int healthMax = 4;
+	public Material full;
+	public Material depleted;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void decreaseHealth(){
+		if(health>0){
+			health--;
+			GameObject heart = this.gameObject.transform.GetChild(0).GetChild(health).gameObject;
+			Renderer rend = heart.GetComponent<Renderer>();
+			rend.enabled = true;
+			rend.sharedMaterial = depleted;
+		}
+		else{
+			//TODO dead!
+		}
+	}
+
+	public void increaseHealth(){
+		if(health < healthMax){
+			GameObject heart = this.gameObject.transform.GetChild(0).GetChild(health).gameObject;
+			Renderer rend = heart.GetComponent<Renderer>();
+			rend.enabled = true;
+			rend.sharedMaterial = full;
+			health++;
+		}
+	}
+}
