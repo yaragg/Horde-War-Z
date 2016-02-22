54493a31500c980931628113d628832ff321c0b0
clairem128 <clm7871@g.rit.edu>
2016-02-22 14:38:57 -0500

    Added files via upload
    Code for the chasing of the character for when they get in a certain distance of the zombie

---
+++ Assets/Chasing.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+public class chasing : MonoBehaviour{
+
+	var Character : Transform;
+	public int MaxDist = 10;
+	public int MinDist = 5;
+
+	void Start (){
+	}
+
+	void Update () 
+	{
+		transform.LookAt(Character);
+
+		if(Vector3.Distance(transform.position,Character.position) >= MinDist){
+
+			transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+			if(Vector3.Distance(transform.position,Character.position) <= MaxDist){
+
+			} 
+
+		}
+	}
