72184db6568deacb73a41114428f74aa7c42d7d0
JarellP4 <jarellp4@yahoo.com>
2016-02-26 14:51:34 -0500

    New branch for assigning random names to characters and zombies
    Also, including the separate colors for characters, so weapon types are
    more easily differentiated.

---
+++ Assets/Scripts/NamesScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NamesScript : MonoBehaviour {
+
+    private static string[] nameBank;
+    private static string[] usedNameBank;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static string NewName()
+    {
+        string name = "";
+
+
+
+
+        return name;
+    }
+
+}
