0a32cf7326cab23504ba74fc3441a2ce154fe946
Zac Cupec <zcupec@gmail.com>
2016-02-26 11:22:10 -0500

    Added Weapon types
    Shotgun fires upwards and downwards improperly

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class Gun : MonoBehaviour {
 
 	public GameObject bulletType;
-    public float fireRate = 10f;
+    
+	public enum weaponType{pistol, sniper, smg, shotgun};
+	public weaponType gunType;
+
+	// Fire Rate
+	float fireRate = 10f;
     float lastFired;
 
 	// Use this for initialization
@@ -14,14 +19,47 @@ public class Gun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		switch (gunType) {
+		case weaponType.pistol: fireRate = 12f;
+			break;
+		case weaponType.shotgun: fireRate = 15f;
+			break;
+		case weaponType.smg: fireRate = 5f;
+			break;
+		case weaponType.sniper: fireRate = 25f;
+			break;
+		}
 	}
 
 	public void Shoot(){
-        if (Time.frameCount - lastFired > fireRate){
-            GameObject bullet = (GameObject)Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(transform.forward, -transform.up));
-            lastFired = Time.frameCount;
-        }
+		switch (gunType) {
+			case weaponType.pistol:
+			if (Time.frameCount - lastFired > fireRate){
+				Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(transform.forward, -transform.up));
+				lastFired = Time.frameCount;
+			}
+			break;
+			case weaponType.shotgun:
+			if (Time.frameCount - lastFired > fireRate){
+				Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(transform.forward, -transform.up));
+				Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(Quaternion.AngleAxis(-15, Vector3.up) * transform.forward, -transform.up));
+				Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(Quaternion.AngleAxis(15, Vector3.up) * transform.forward, -transform.up));
+				lastFired = Time.frameCount;
+			}
+			break;
+			case weaponType.smg:
+			if (Time.frameCount - lastFired > fireRate){
+				Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(transform.forward, -transform.up));
+				lastFired = Time.frameCount;
+			}
+			break;
+			case weaponType.sniper:
+			if (Time.frameCount - lastFired > fireRate){
+				Instantiate(bulletType, this.transform.position, Quaternion.LookRotation(transform.forward, -transform.up));
+				lastFired = Time.frameCount;
+			}
+			break;
+		}
 		//TODO change to transform.forward and transform.up once the forward vector is set properly
 	}
 }
