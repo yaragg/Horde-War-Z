c6ef3b6f2f4f8236844a929fff4094abe14db645
Yara Grassi Gouffon <yara.gouffon@usp.br>
2016-04-03 00:47:34 -0400

    Added a scrollable panel that displays one randomly chosen story, with the characters' names added in.

--- Assets/Scripts/StoryScript.cs
+++ Assets/Scripts/StoryScript.cs
@@ -1,19 +1,32 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class StoryScript : MonoBehaviour {
 
-	public GameObject test;
+	public GameObject storyContent;
+	public string[] stories;
+
+	void Awake(){
+		TextAsset storyFile = Resources.Load("stories") as TextAsset;
+        stories = storyFile.text.Split('#');
+        Resources.UnloadAsset(storyFile);
+	}
 
 	// Use this for initialization
 	void Start () {
-        Text testDisplay = test.GetComponent<Text>();
-        testDisplay.text = "";
-		for(int i=0; i<4; i++){
-			testDisplay.text += NameScript.PlayerNames[i] + " ";
+        Text storyDisplay = storyContent.GetComponent<Text>();
 
-		}
+        //Pick one of the stories
+		int index = Random.Range(0, stories.Length);
+		storyDisplay.text = stories[index];
+
+		//Add in generated player names
+		storyDisplay.text = storyDisplay.text.Replace("[A]", NameScript.PlayerNames[0]);
+		storyDisplay.text = storyDisplay.text.Replace("[B]", NameScript.PlayerNames[1]);
+		storyDisplay.text = storyDisplay.text.Replace("[C]", NameScript.PlayerNames[2]);
+		storyDisplay.text = storyDisplay.text.Replace("[D]", NameScript.PlayerNames[3]);
 	}
 	
 	// Update is called once per frame
